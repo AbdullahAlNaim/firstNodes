2e26447edf460a58af1cb0f74cf9fa6d005ff9d5
Abdullah Naim <aanaim96@gmail.com>
2023-01-31 22:41:46 -0500

    Add files via upload

---
+++ nodes/index.js
@@ -0,0 +1,9 @@
+const langs = require('langs');
+// const franc = require('franc');
+// import * as langs from 'langs';
+import { franc, francAll } from 'franc'
+const args = process.argv.slice(2);
+
+for (let arg of args) {
+    console.log(arg);
+}
